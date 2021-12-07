c28ab692b2dbe7b885bf04f4a50b1a5110a2bd16
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-07 14:43:58 -0800

    adding filters

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
 import './Main.css';
 import Country from '../Country/Country';
 
 export default function Main({ countries }) {
+  const [query, setQuery] = useState([]);
+  const [continent, setContinent] = useState('All');
+
+  function filterCountries() {
+    return countries.filter((country) => {
+      return (
+        country.name.includes(query) && (country.continent === continent || continent === 'All')
+      );
+    });
+  }
+
   return (
-    <div className="main">
-      {countries.map((country) => {
-        return <Country key={country.id} {...country} />;
-      })}
-    </div>
+    <>
+      <div className="filters">
+        <input
+          className="search"
+          type="text"
+          placeholder="Search countries"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <select value={continent} onChange={(e) => setContinent(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Antarctica">Antarctica</option>
+          <option value="Asia">Asia</option>
+          <option value="Australia">Australia</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="South America">South America</option>
+        </select>
+      </div>
+      <div className="main">
+        {filterCountries().map((country) => {
+          return <Country key={country.id} {...country} />;
+        })}
+      </div>
+    </>
   );
 }
