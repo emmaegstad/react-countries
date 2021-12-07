c4693cbf106bcf7db02e4de23d0d8f28f38f9918
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-07 12:45:27 -0800

    adding images from flagpedia

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Country.css';
 
-export default function Country({ name }) {
-  return <div className="country">{name}</div>;
+export default function Country({ name, iso2 }) {
+  return (
+    <div className="country">
+      <img
+        src={`https://flagcdn.com/60x45/${iso2.toLowerCase()}.png`}
+        width="60"
+        height="45"
+        alt={name}
+      />
+      <p>{name}</p>
+    </div>
+  );
 }
